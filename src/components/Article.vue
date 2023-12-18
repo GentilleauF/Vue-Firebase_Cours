c3055c7366e126561f73adfafa97636584b1e012
GentilleauF <francois.gentilleau@gmail.com>
2023-12-18 16:58:54 +0100

    fin de journee

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,3 +1,38 @@
+<script>
+import articleDataService from "../services/ArticleDataService";
+
+export default {
+  name: article,
+  props: [article],
+
+  data() {
+    return {
+      currentArticle: null,
+      message: "",
+    };
+  },
+
+  watch: {
+    article(article) {
+      this.currentArticle = { ...article };
+      this.message = "";
+    },
+  },
+
+  methods: {
+    updatePublished(status) {
+      articleDataService
+        .update(this.currentArticle.key, { published: status })
+        .then(() => {
+            this.currentArticle.published = status;
+            this.message = "le status a été MAJ"
+        })
+        .catch((e)=>console.log(e));
+    },
+  },
+};
+</script>
+
 <template>
-    <h1>article</h1>
+  <h1>article</h1>
 </template>
